ac88e7e343831e7d531d5487fd7465ade1a10056
redouane ayadi <redouaneayadi98@gmail.com>
2023-01-24 22:48:29 +0100

    main file

---
+++ src/test/java/com/ecomm_backend/EcommBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
